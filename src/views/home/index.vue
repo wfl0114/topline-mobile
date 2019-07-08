634aeae5b772827e3a0ab0a464a32c8fe2de806d
wfl <wang@123.com>
2019-07-08 22:56:34 +0800

    准备两个页面组件

---
+++ src/views/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Home Component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIndex',
+  data () {
+    return {}
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Login Component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {}
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
