65d34c7a361ad74e243b73fd3352695c951fa10c
wfl <wang@123.com>
2019-07-12 09:15:12 +0800

    配置像素转REM

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <p>Home Component</p>
+    <div class="box"></div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
@@ -17,4 +22,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.box {
+  width: 375px;
+  height: 100px;
+  background-color: red;
+}
 </style>
